b18a7c2816bf53382a749574e033a7324c88bf44
꼬기 <ria9810@gmail.com>
2020-10-11 23:13:02 +0900

    20201011 vue router 설치

--- mysite/src/main.js
+++ mysite/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import router from './router'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')

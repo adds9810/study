e06f6f886289c0665eaf84ba0a8dd9e3b720b55e
꼬기 <ria9810@gmail.com>
2020-10-12 23:06:11 +0900

    201012 vue route 공부

--- mysite/src/router.js
+++ mysite/src/router.js
@@ -2,3 +2,16 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter);
+
+const routes =[{}]
+
+const router = new VueRouter({
+    mode:'history',
+    //base:'/ux/kjh/vue/',
+    routes,
+    scrollBehavior () {
+        return { x: 0, y: 0 }
+    }
+})
+
+export default router;

b2e91285643e5c053137c6f6022d0b7d7bb9eb8c
꼬기 <ria9810@gmail.com>
2020-10-14 23:11:38 +0900

    201014 vuex 추가

--- mysite/src/store.js
+++ mysite/src/store.js
@@ -1,1 +1,12 @@
-state
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state,
+    getters,
+    mutations,
+    actions,
+    strict: process.env.NODE_ENV !== 'production'
+})

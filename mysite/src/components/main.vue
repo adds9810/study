4bbd87f15076a918e41185123e62c8dfc4aba84b
꼬기 <ria9810@gmail.com>
2020-10-18 23:48:39 +0900

    201018 vue 공부

---
+++ mysite/src/components/main.vue
@@ -0,0 +1,12 @@
+<template>
+  <section id="main">
+    main
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'main',
+  methods:{}
+}
+</script>
---
+++ mysite/src/components/sub.vue
@@ -0,0 +1,10 @@
+<template>
+  <section id="sub"></section>
+</template>
+
+<script>
+export default {
+  name: 'sub',
+  methods:{}
+}
+</script>

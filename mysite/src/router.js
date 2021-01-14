2c28c83fe1afbd9bc3074563ed790579f4c3363f
꼬기 <ria9810@gmail.com>
2021-01-14 19:12:16 +0900

    flex 간단한 설명

--- mysite/src/router.js
+++ mysite/src/router.js
@@ -4,7 +4,6 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter);
 
 const routes =[{
-    {
         path:'',
         component: () => import( '@/components/layout.vue'),
         children: [
